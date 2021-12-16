b1c9fbdc573988899a963ee8b68b3a4d7628e5e1
Murat Aksoy <murat@ionpath.com>
2021-12-15 21:36:27 -0800

    day14 done

--- day14.cpp
+++ day14.cpp
@@ -12,31 +12,25 @@
 
 using namespace std;
 
-string pair_insertion(string polymer, map<string, char> rules)
+map<string, long long> split(map<string, long long>& tuple_freqs, map<string, char>& rules)
 {
-    int n = polymer.size();
-    string polymer_inserted;
-    for (int i = 0; i< n-1;i++)
+    map<string, long long> tuple_freqs_new;
+    for (auto const & it: tuple_freqs)
     {
-
-        polymer_inserted.push_back(polymer[i]);
-        string rule_key = polymer.substr(i, 2);
-        polymer_inserted.push_back(rules[rule_key]);
-
+        if (it.second == 0) continue;
+        char new_char = rules[it.first];
+        string str;
+        str.push_back(it.first[0]);
+        str.push_back(new_char);
+        if (tuple_freqs_new.find(str) == tuple_freqs_new.end()) tuple_freqs_new[str] = 0;
+        tuple_freqs_new[str]+=it.second;
+        str.clear();
+        str.push_back(new_char);
+        str.push_back(it.first[1]);
+        if (tuple_freqs_new.find(str) == tuple_freqs_new.end()) tuple_freqs_new[str] = 0;
+        tuple_freqs_new[str] += it.second;
     }
-    polymer_inserted.push_back(polymer[n-1]);
-    return polymer_inserted;
-}
-
-vector<int> find_occurrence(string str)
-{
-    vector<int> occurrences(26, 0);
-    for (auto c: str)
-    {
-        occurrences[c-'A']++;
-    }
-    return occurrences;
-
+    return tuple_freqs_new;
 }
 
 int main()
@@ -46,25 +40,51 @@ int main()
     string str;
     string polymer;
     getline(f, polymer);
-    getline(f, str);
+    map<string, long long> tuple_freqs;
+    for (int i = 0; i < polymer.size()-1; i++)
+    {
+        auto str_tuple = polymer.substr(i, 2);
+        if (tuple_freqs.find(str_tuple) == tuple_freqs.end())
+        {
+            tuple_freqs[str_tuple] = 1;
+        }
+        else
+        {
+            tuple_freqs[str_tuple]++;
+        }
+    }
+
+    getline(f, str); // blank
+
+    // getting the rules
     map<string, char> rules;
     while(getline(f, str))
     {
         rules[str.substr(0, 2)] = str[6];
     }
 
-    for (int i=0;i<10;i++)
+    int N = 40;
+    for (int i = 0; i< N; i++)
     {
-        cout << i << endl;
-        polymer = pair_insertion(polymer, rules);
-        //cout << polymer << endl;
+        tuple_freqs = split(tuple_freqs, rules);
     }
 
-    auto occurrences = find_occurrence(polymer);
-    sort(occurrences.begin(), occurrences.end());
+    vector<long long> freqs(26, 0);
+    freqs[polymer[0]-'A']++;
+    freqs[polymer[polymer.size()-1]-'A']++;
+    // finding frequencies
+    for (auto const & it: tuple_freqs)
+    {
+        freqs[it.first[0]-'A']+=it.second;
+        freqs[it.first[1]-'A']+=it.second;
+    }
+
+    sort(freqs.begin(), freqs.end());
     int ind = 0;
-    while(occurrences[ind] == 0) ind++;
-    cout << occurrences[occurrences.size()-1] - occurrences[ind] << endl;
+    while (freqs[ind] == 0) ind++;
+    cout << (freqs[freqs.size()-1] - freqs[ind]) / 2 << endl;
+
+
 
 
 
